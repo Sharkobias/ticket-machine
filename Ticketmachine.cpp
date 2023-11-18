996edc010456321b4ae2ecb932627d86573604e1
egzotic <toby.fajks@gmail.com>
2023-11-18 21:25:45 +0100

    fixed the cout displaying change

--- Ticketmachine.cpp
+++ Ticketmachine.cpp
@@ -148,7 +148,10 @@ int main()
             cout << "[1zl]" << endl << "[2zl]" << endl << "[5zl]" << endl << "[10zl]" << endl << "[20zl]" << endl << "[50zl]" << endl;
             cin >> moneygiven;
             remaining_cost -= coin_values[moneygiven];
-            cout <<lang[18]<< remaining_cost / 100.0 << "zl" << endl;
+            if (remaining_cost > 0)
+            {
+                cout << lang[18] << remaining_cost / 100.0 << "zl" << endl;
+            }
         } while (remaining_cost > 0);
         if (remaining_cost < 0) 
         {
@@ -163,20 +166,8 @@ int main()
                 }
             } while (true_rest > 0);
         }
+        cout << lang[11] << endl;
+        cout << " " << endl;
+        cout << lang[14] << endl;
     }
-    
 }
-
-
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
