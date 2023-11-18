778834041bf11e6f11efdf02e64cae2cc5595aa5
egzotic <toby.fajks@gmail.com>
2023-11-18 21:15:01 +0100

    added map with Polish language support:D

--- Ticketmachine.cpp
+++ Ticketmachine.cpp
@@ -10,6 +10,52 @@ string moneygiven;
 
 int main()
 {
+    map<int, string> english = {
+        {1, "What kind of ticket do you want to purchase?:"},
+        {2, "[1]Normal Ticket"},
+        {3, "[2]Reduced Ticket"},
+        {4, "[3]Time Ticket"},
+        {5, "Choose type of payment:"},
+        {6, "[1]Card"},
+        {7, "[2]Blik"},
+        {8, "[3]Cash"},
+        {9, "cost:"},
+        {10, "Please insert card"},
+        {11, "Please wait..." },
+        {12, "authorization completed"},
+        {13, "authorization canceled"},
+        {14, "Thanks for using our services!"},
+        {15,  "Please, enter the blik code:" },
+        {16,  "insert coins or cash" },
+        {17, "insert:"},
+        {18,  "Money left to pay:" },
+        {19, "Your Change:"}
+    };
+
+    map<int, string> polish = {
+        {1, "Wybierz typ biletu:"},
+        {2, "[1]Normalny"},
+        {3, "[2]Ulgowy"},
+        {4, "[3]Okresowy"},
+        {5, "Wybierz typ platnosci:"},
+        {6, "[1]Karta"},
+        {7, "[2]Blik"},
+        {8, "[3]Gotowka"},
+        {9, "koszt:"},
+        {10, "Prosimy o wprowadzenie karty"},
+        {11, "Prosimy czekac... "},
+        {12, "autoryzacja ukonczona"},
+        {13, "autoryzacja odrzucona"},
+        {14, "dziekujemy za skorzystanie z naszego serwisu!"},
+        {15, "Prosimy o wpisanie kodu Blik:"},
+        {16, "wloz monety"},
+        {17, "wprowadz:"},
+        {18, "Pozostalo do zaplaty:"},
+        {19, "reszta:"}
+    };
+
+    map<int, string> lang;
+
     map<string, int> coin_values = {
         {"50zl", 5000},
         {"20zl", 2000},
@@ -33,85 +79,92 @@ int main()
 
     cout << "Hello!" << endl;
     cout << "Select language:" << endl;
-    cout << "[1]English" << endl << "[2]Polski" << endl << "[3]українська" << endl << "[4]Deutsch" << endl;
+    cout << "[1]English" << endl << "[2]Polski" << endl;
     cin >> input;
     if (input == "1")
     {
-        cout << "What kind of ticket do you want to purchase?:" << endl;
-        cout << "[1]Normal Ticket" << endl << "[2]Reduced Ticket" << endl << "[3]Time ticket" << endl;
+        lang = english;
+    }
+    else if (input == "2")
+    {
+        lang = polish;
+    }
+    
+    cout <<lang[1]<< endl;
+    cout <<lang[2]<< endl<<lang[3]<<endl<<lang[4]<< endl;
+    cin >> input;
+    if (input == "1") {
+        ticket_cost = norm;
+    }
+    else if (input == "2") {
+        ticket_cost = redu;
+    }
+    else if (input == "3") {
+        ticket_cost = timsdob;
+    }
+    ticket_cost_zl = ticket_cost / 100.0;
+    cout <<lang[5]<< endl;
+    cout << lang[6]<< endl <<lang[7]<< endl <<lang[8]<< endl;
+    cin >> input;
+    if (input == "1")
+    {
+        cout <<lang[9]<<ticket_cost_zl<< endl;
+        cout <<lang[10]<< endl;
+        cout << "[a]ccept" << endl << "[d]ecline" << endl;
         cin >> input;
-        if (input == "1") {
-            ticket_cost = norm;
-        }
-        else if (input == "2") {
-            ticket_cost = redu;
-        }
-        else if (input == "3") {
-            ticket_cost = timsdob;
-        }
-        ticket_cost_zl = ticket_cost / 100.0;
-        cout << "Choose type of payment:" << endl;
-        cout << "[1]Card" << endl << "[2]blik" << endl << "[3]cash" << endl;
-        cin >> input;
-        if (input == "1")
+        if (input == "a")
         {
-            cout << "cost:" <<ticket_cost_zl<< endl;
-            cout << "Please insert card" << endl;
-            cout << "[a]ccept" << endl << "[d]ecline" << endl;
-            cin >> input;
-            if (input == "a")
-            {
-                cout << "Please wait..." << endl;
-                cout << " " << endl;
-                cout << "authorization completed" << endl;
-                cout << "Thanks for using our services!" << endl;
-
-            }
-            else if (input == "d")
-            {
-                cout << "authorization canceled" << endl;
-            }
-
-        }
-        else if (input == "2")
-        {
-            cout << "cost:" << ticket_cost_zl<< endl;
-            cout << "Please, enter the blik code:" << endl;
-            cin >> input;
-            cout << "Please wait..." << endl;
+            cout <<lang[11]<< endl;
             cout << " " << endl;
-            cout << "authorisation completed" << endl;
-            cout << "Thanks for using our services!" << endl;
+            cout <<lang[12] << endl;
+            cout <<lang[14]<< endl;
+
         }
-        else if (input == "3")
+        else if (input == "d")
         {
-            cout << "cost:"<<ticket_cost_zl<<endl;
-            remaining_cost = ticket_cost;
+            cout <<lang[13]<< endl;
+        }
+
+    }
+    else if (input == "2")
+    {
+        cout <<lang[9]<< ticket_cost_zl<< endl;
+        cout <<lang[15]<< endl;
+        cin >> input;
+        cout <<lang[11]<< endl;
+        cout << " " << endl;
+        cout <<lang[12]<< endl;
+        cout <<lang[14]<< endl;
+    }
+    else if (input == "3")
+    {
+        cout <<lang[9]<<ticket_cost_zl<<endl;
+        remaining_cost = ticket_cost;
+        do
+        {
+            cout <<lang[16]<< endl;
+            cout <<lang[17]<< endl;
+            cout << "[20gr]" << endl << "[50gr]" << endl;
+            cout << "[1zl]" << endl << "[2zl]" << endl << "[5zl]" << endl << "[10zl]" << endl << "[20zl]" << endl << "[50zl]" << endl;
+            cin >> moneygiven;
+            remaining_cost -= coin_values[moneygiven];
+            cout <<lang[18]<< remaining_cost / 100.0 << "zl" << endl;
+        } while (remaining_cost > 0);
+        if (remaining_cost < 0) 
+        {
+            true_rest = remaining_cost * (-1);
             do
             {
-                cout << "insert coins or cash" << endl;
-                cout << "Insert:" << endl;
-                cout << "[20gr]" << endl << "[50gr]" << endl;
-                cout << "[1zl]" << endl << "[2zl]" << endl << "[5zl]" << endl << "[10zl]" << endl << "[20zl]" << endl << "[50zl]" << endl;
-                cin >> moneygiven;
-                remaining_cost -= coin_values[moneygiven];
-                cout << "Money left to pay:" << remaining_cost / 100.0 << "zl" << endl;
-            } while (remaining_cost > 0);
-            if (remaining_cost < 0) 
-            {
-                true_rest = remaining_cost * (-1);
-                do
-                {
-                    for (auto coin : coin_values) {
-                        if (true_rest - coin.second >= 0) {
-                            cout << "your change:" << coin.first << endl;
-                            true_rest -= coin.second;
-                        }
+                for (auto coin : coin_values) {
+                    if (true_rest - coin.second >= 0) {
+                        cout <<lang[19]<< coin.first << endl;
+                        true_rest -= coin.second;
                     }
-                } while (true_rest > 0);
-            }
+                }
+            } while (true_rest > 0);
         }
     }
+    
 }
 
 
